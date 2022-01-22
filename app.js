e11ea46c0dde3206393a4b4ec7408ba5444066b5
manuyeregui <manuelyeregui@gmail.com>
2022-01-22 03:31:58 -0300

    agregando carpetas

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+
+app.listen(3000, () => {
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+});
